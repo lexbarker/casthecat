bf1b576b0786eb603e5477c9a760eca0395a4231
lexbarker <42436640+lexbarker@users.noreply.github.com>
2020-03-24 14:14:47 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 
 
 // view engine setup
